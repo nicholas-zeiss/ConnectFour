21a6b357ee26db66dc9189a32bab66fab987d2fb
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-07-04 18:02:43 -0400

    added some new css, added a navbar at top of page to choose between connect 4 and game of life, game of life now wraps around edges

--- app/client/Game of Life/LifeView.js
+++ app/client/Game of Life/LifeView.js
@@ -21,6 +21,20 @@ const LifeView = React.createClass({
 		this.draw();
 	},
 
+	// draw() {
+	// 	let ctx = this.state.container;
+
+	// 	ctx.fillStyle = '#3a3a3a';
+	// 	ctx.fillRect(0, 0, this.state.width * this.props.cells[0].length, this.state.height * this.props.cells.length);			//set background color
+
+	// 	for (let j = 0; j < this.props.cells.length; j++) {
+	// 		for (let i = 0; i < this.props.cells[0].length; i++) {
+	// 			ctx.fillStyle = this.props.cells[j][i] ? 'rgb(67,211,0)' : 'black';																			//green if alive, black if dead
+	// 			ctx.fillRect(i * this.state.width + 1, j * this.state.height + 1, this.state.width - 2, this.state.height - 2);
+	// 		}
+	// 	}
+	// },
+
 	draw() {
 		let ctx = this.state.container;
 
@@ -31,10 +45,29 @@ const LifeView = React.createClass({
 			for (let i = 0; i < this.props.cells[0].length; i++) {
 				ctx.fillStyle = this.props.cells[j][i] ? 'rgb(67,211,0)' : 'black';																			//green if alive, black if dead
 				ctx.fillRect(i * this.state.width + 1, j * this.state.height + 1, this.state.width - 2, this.state.height - 2);
+
+				//adds a simple lighting effect
+				let x = (i + .5) * this.state.width - 40, y = (j + .5) * this.state.height - 40;
+				let gradient = ctx.createRadialGradient(80, 80, 80, 80, 80, 0);
+				gradient.addColorStop(0, 'rgba(67,211,0,0)');
+				gradient.addColorStop(1, 'rgba(67,211,0,.5)');
+				ctx.fillStyle = gradient;
+				ctx.fillRect(x, y, 80, 80);
+			}
+		}
+
+		//adds a crude lighting
+		for (let j = 0; j < this.props.cells.length; j++) {
+			for (let i = 0; i < this.props.cells[0].length; i++) {
+				if (this.props.cells[j][i]) {
+					let x = (i + .5) * this.state.width, y = (j + .5) * this.state.height;
+					let gradient = ctx.createRadialGradient(x, y, 75, x, y, 15);
+
+				}
 			}
 		}
 	},
-	
+
 	clickCell(e) {
 		this.props.toggleCell(Math.floor(e.nativeEvent.offsetY / this.state.height), 	//row and column respectively
 													Math.floor(e.nativeEvent.offsetX / this.state.width));
