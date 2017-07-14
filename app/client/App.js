a33fca942a77e2198340b5a32f2f6485576845da
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-07-13 22:15:25 -0400

    remove all game of life components which now reside in a separate repository

--- app/client/App.js
+++ app/client/App.js
@@ -2,8 +2,8 @@
 initializes the react app and renders it to the DOM
 **/
 
-import Choose from './Choose';
+import Game from './components/Game';
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<Choose/>, document.getElementById('root'));
+render(<Game/>, document.getElementById('root'));
