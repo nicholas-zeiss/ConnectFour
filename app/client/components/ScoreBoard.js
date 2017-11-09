302ee413100969f6833370a93594a0b50c2438ee
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-11-08 21:18:40 -0500

    cleaned up style issues in ScoreBoard component

--- app/client/components/ScoreBoard.js
+++ app/client/components/ScoreBoard.js
@@ -1,5 +1,7 @@
 /**
-Renders the top 10 scores of all time, child of the Game component.
+ *
+ *	Renders the top 10 scores of all time, child of the Game component.
+ *
 **/
 
 import React from 'react';
@@ -7,6 +9,7 @@ import React from 'react';
 
 const toggle = () => {
 	let board = document.getElementById('scoresContainer');
+
 	if (board.style.right == '0px') {
 		board.style.right = '295px';
 	} else {
@@ -14,34 +17,33 @@ const toggle = () => {
 	}
 };
 
-const ScoreBoard = props => {
 
-	return (
-		<div id='scoresContainer' onClick={toggle}>
-			<div id='leaderboard'>
-				<h1>Top 10 Games</h1>
-				<table id='scoreTable'>
-					<tbody>
-						{
-							props.scores.map(score => (
-								<tr key={score.id}>
-									<td>{score.id}.</td>
-									<td>{score.name}</td>
-									<td>{score.outcome}&nbsp;&nbsp;</td>
-									<td>{score.turns}&nbsp;</td>
-									<td>{score.date}</td>
-								</tr>
-							))
-						}
-					</tbody>
-				</table>
-			</div>
-			<div id='expand'>
-				&#8811;
-			</div>
+const ScoreBoard = props => (
+	<div id='scoresContainer' onClick={ toggle }>
+		<div id='leaderboard'>
+			<h1> Top 10 Games </h1>
+			
+			<table id='scoreTable'>
+				<tbody>
+					{
+						props.scores.map(score => (
+							<tr key={ score.id }>
+								<td>{ score.id }.</td>
+								<td>{ score.name }</td>
+								<td>{ score.outcome }&nbsp;&nbsp;</td>
+								<td>{ score.turns }&nbsp;</td>
+								<td>{ score.date }</td>
+							</tr>
+						))
+					}
+				</tbody>
+			</table>
 		</div>
-	);
-}
+		
+		<div id='expand'> &#8811; </div>
+	</div>
+);
+
 
 export default ScoreBoard;
 
