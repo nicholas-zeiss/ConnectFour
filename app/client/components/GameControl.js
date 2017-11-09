2a2ac8fa859bc0c2e89466555288e9de0832e050
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-11-08 21:11:14 -0500

    cleaned up style issues in GameControl component

--- app/client/components/GameControl.js
+++ app/client/components/GameControl.js
@@ -1,41 +1,43 @@
 /**
-Lets the user start a new game or, if eligible, choose to submit their score by opening the modal. Child of the Game component.
+ *
+ *	Lets the user start a new game or, if eligible, choose to submit their score by opening the modal. Child of the Game component.
+ *
 **/ 
 
 import React from 'react';
 
 
-const GameControl = (props) => {
-
+const GameControl = props => {
 	const onClick = e => {
-		e.persist();
-		if (e.target.id == 'newGame') {
-			props.clearBoard();
-		} else {
-			props.showModal();
-		}
-		
-		e.target.style.transform = 'translate(2px, 2px)';
-		e.target.style.backgroundColor = '#106333';
+		const target = e.target;
 
-		setTimeout(() => e.target.style.transform = 'translate(0, 0)', 60);
-		setTimeout(() => e.target.style.backgroundColor = '#1e9e53', 100);
-	}
+		target.id == 'newGame' ? props.clearBoard() : props.showModal();
+		target.style.transform = 'translate(2px, 2px)';
+		target.style.backgroundColor = '#106333';
+
+		setTimeout(() => target.style.transform = 'translate(0, 0)', 60);
+		setTimeout(() => target.style.backgroundColor = '#1e9e53', 100);
+	};
 
 	return (
 		<div id='gameControl'>
 			<button
-				type='button'
+				disabled={ !props.eligible }
 				id='submitScore'
-				disabled={!props.eligible}
-				style={{visibility: props.eligible ? 'visible' : 'hidden'}}
-				onClick={onClick}>
+				onClick={ onClick }
+				style={ { visibility: props.eligible ? 'visible' : 'hidden' } }
+				type='button'
+			>
 				Submit Score
 			</button>
-			<button id='newGame' onClick={onClick}>Reset</button>
+
+			<button id='newGame' onClick={ onClick }>
+				Reset
+			</button>
 		</div>
 	);
-}
+};
+
 
 export default GameControl;
 			
