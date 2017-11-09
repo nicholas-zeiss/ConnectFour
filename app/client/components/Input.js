b725e28dcd119f7c8601b503849b9fd707c2df98
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-11-08 21:16:46 -0500

    cleaned up style issues in Input component

--- app/client/components/Input.js
+++ app/client/components/Input.js
@@ -1,6 +1,8 @@
 /**
-Responsible for handling user input for selecting a move through buttons; the event listener for the corresponding keypresses
-are instantiated in the parent component Game.
+ *
+ *	Responsible for handling user input for selecting a move through buttons; the event listener for the corresponding keypresses
+ *	are instantiated in the parent component Game and create a click event for these buttons.
+ *
 **/
 
 import React from 'react';
@@ -8,36 +10,38 @@ import React from 'react';
 
 const Input = (props) => {
 	
+	const click = (i, e) => {
+		const target = e.target;
 
-	const onClick = (i, e) => {
-		e.persist();
+		target.style.transform = 'translate(2px, 2px)';
+		target.style.backgroundColor = '#106333';
+		setTimeout(() => target.style.transform = 'translate(0, 0)', 60);
+		setTimeout(() => target.style.backgroundColor = '#1e9e53', 100);
 		
-		props.makeMove.call(null, i);
-
-		e.target.style.transform = 'translate(2px, 2px)';
-		e.target.style.backgroundColor = '#106333';
-
-		setTimeout(() => e.target.style.transform = 'translate(0, 0)', 60);
-		setTimeout(() => e.target.style.backgroundColor = '#1e9e53', 100);
-	}
+		props.makeMove(i);
+	};
 
 	return (
 		<div id = 'inputContainer'>
 			{
-				new Array(7).fill(1).map((el, i) => 
-					<button 
-						type='button' 
-						id={'col-button--' + (i + 1)} 
-						key={i} 
-						onClick={onClick.bind(null, i)} 
-						disabled={props.inputLock}>
-						{i + 1}
-					</button>
-				)
-		  }
+				new Array(7)
+					.fill(1)
+					.map((el, i) => 
+						<button 
+							disabled={ props.inputLock }
+							id={ 'col-button--' + (i + 1) } 
+							key={ i } 
+							onClick={ click.bind(null, i) } 
+							type='button' 
+						>
+							{ i + 1 }
+						</button>
+					)
+			}
 		</div>
 	);
-}
+};
+
 
 export default Input;
 
